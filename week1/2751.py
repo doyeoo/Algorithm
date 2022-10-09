b24e738ccd314033bca8d4cd997190b72a22a905
doyeoo <ehdusla013@gmail.com>
2022-10-09 22:02:02 +0900

    weel1: 내장 함수 정렬 BOJ 2751

--- week1/2751.py
+++ week1/2751.py
@@ -1,32 +1,11 @@
-# -*- coding: utf-8 -*-
+import sys
 
 n = int(input())
 arr = []
 
-for i in range(n):
-    arr.append(int(input()))
+for _ in range(n):
+    arr.append(int(sys.stdin.readline()))
+for num in sorted(arr):
+    print(num)
 
-#quick sort
-def sort(array,start,end):
-    if start>= end:       
-        return
-    pivot = start           
-    left = start + 1
-    right = end
-    
-    while left<=right: 
-        while left<=end and array[left]<=array[pivot]:
-            left +=1
-        while right>start and array[right]>= array[pivot]:
-            right-=1
-        if left>right:
-            array[right],array[pivot]= array[pivot],array[right]
-        else:           
-            array[left],array[right]=array[right],array[left]
-    sort(array, start,right-1)
-    sort(array, right+1, end)
 
-sort(arr, 0, n-1)
-
-for i in range(n):
-    print(arr[i])
